feb275e337bc6c75ede53a037de765ecdf87b335
Marco <marcohavermans+github@hotmail.nl>
2017-06-08 10:22:45 +0200

    Added support for websocket.

--- Triangle Streaming Server/Program.cs
+++ Triangle Streaming Server/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
 
 namespace Triangle_Streaming_Server
 {
@@ -10,6 +13,31 @@ namespace Triangle_Streaming_Server
 	{
 		static void Main(string[] args)
 		{
+			WebSocketServer webSocketServer = new WebSocketServer();
+			webSocketServer.AddWebSocketService<VideoStream>("/stream");
+			webSocketServer.Start();
+			Console.ReadKey(true);
+			webSocketServer.Stop();
+		}
+	}
+
+	public class VideoStream : WebSocketBehavior
+	{
+		protected override void OnMessage(MessageEventArgs e)
+		{
+			// this.ID can be used for uniquely identifying sessions 
+
+			// Here all the received data should be read
+			if (e.IsBinary)
+			{
+				// Received probably camera bytes
+				byte[] receivedBytes = e.RawData;
+
+				// Do somehting with them
+			}
+
+			// Send can be used to send data to other devices.
+			//Send(fileBytes);
 		}
 	}
 }
