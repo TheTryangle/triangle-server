3a149ce5055deac404f636c9f2090a175012fae5
Marco <marcohavermans+github@hotmail.nl>
2017-06-08 12:46:41 +0200

    Added working of receiving stream and resending the stream.

---
+++ Triangle Streaming Server/ReceiveStream.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using WebSocketSharp.Server;
+
+namespace Triangle_Streaming_Server
+{
+	public class ReceiveStream : WebSocketBehavior
+	{
+		protected override void OnOpen()
+		{
+			Console.WriteLine($"Someone connected to receive, {this.Context.UserEndPoint.ToString()}");
+		}
+	}
+}
---
+++ Triangle Streaming Server/WebSocketManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp.Server;
+
+namespace Triangle_Streaming_Server
+{
+	public class WebSocketManager : IDisposable
+	{
+		private static WebSocketManager mInstance;
+
+		public static WebSocketManager GetInstance()
+		{
+			if (mInstance == null)
+				mInstance = new WebSocketManager();
+
+			return mInstance;
+		}
+
+		public WebSocketServer Server { get; set; }
+
+		private WebSocketManager()
+		{
+			InitializeServer();
+		}
+
+		private void InitializeServer()
+		{
+			Server = new WebSocketServer(1234);
+			Server.AddWebSocketService<VideoStream>("/send");
+			Server.AddWebSocketService<ReceiveStream>("/receive");
+
+			Server.Start();
+		}
+
+		public void Dispose()
+		{
+			Server.Stop();
+		}
+	}
+}
