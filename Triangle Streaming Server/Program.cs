b51b3f5e1dc8b18f65eaeec0412d911056ca99a4
Kevin Ly <kevinly92@hotmai.com>
2017-06-16 14:41:04 +0200

    streamcontroller

--- Triangle Streaming Server/Program.cs
+++ Triangle Streaming Server/Program.cs
@@ -8,7 +8,7 @@ namespace Triangle_Streaming_Server
 	{
 		static void Main(string[] args)
 		{
-            string baseAddressWebApi = "http://localhost:9000/";
+            string baseAddressWebApi = "http://145.49.44.137:9000/";
             WebSocketManager webManager = WebSocketManager.GetInstance();
             WebApp.Start<WebApiStart>(baseAddressWebApi);
             Console.WriteLine("Press any key to close server.");
--- Triangle Streaming Server/WebApi/StreamController.cs
+++ Triangle Streaming Server/WebApi/StreamController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -24,10 +25,24 @@ namespace Triangle_Streaming_Server.WebApi
         }
         
         [HttpPut]
-        public async void Send(Guid id)
+        public async Task<IHttpActionResult> Send(Guid id)
         {
-            byte[] data = await Request.Content.ReadAsByteArrayAsync();
-            StreamQueueManager.GetInstance().AddToQueue(id.ToString(), data);
+            var provider = new MultipartMemoryStreamProvider();
+
+            await Request.Content.ReadAsMultipartAsync(provider);
+
+            if (provider.Contents.Count == 0) return InternalServerError(new Exception("Upload failed"));
+
+            var file = provider.Contents[0]; // if you handle more then 1 file you can loop provider.Contents
+
+            var buffer = await file.ReadAsByteArrayAsync();
+
+            // .. do whatever needed here
+            StreamQueueManager.GetInstance().AddToQueue(id.ToString(), buffer);
+
+
+            return Ok();
+
         }
     }
 }
