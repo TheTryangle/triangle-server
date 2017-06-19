67e72e08d1e2a7515f871d08e0c6e77227c4c313
Marco Havermans <marcohavermans+github@hotmail.nl>
2017-06-19 20:17:58 +0200

    Added print lines

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,16 @@
 # Create publish artifact
 dotnet publish -c Release src
 
+printf 'Zipping files'
 #Zip the files
 tar -zcvf package.tgz src/bin/Release/netcoreapp1.0/publish/
-
+printf 'done zipping files'
+printf 'sending file to deploy'
 export SSHPASS=$DEPLOY_PASS
 sshpass -e scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+printf 'file sent'
 
+printf 'deploying with SSH'
 eval 'sshpass -e ssh "$DEPLOY_USER"@"$DEPLOY_HOST" << EOF
 cd `dirname "$DEPLOY_PATH"`
 tar -xzf package.tgz build
@@ -15,3 +19,4 @@ rm -rf www_old
 mv "$DEPLOY_PATH" www_old
 mv build "$DEPLOY_PATH"
 EOF'
+printf 'done deploying'
