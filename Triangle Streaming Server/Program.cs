94dccdc6d895c7f2f52d538b40565c09c30ce8f8
Kevin Ly <kevinly92@hotmai.com>
2017-06-19 11:49:28 +0200

    Revert "Merge branch 'develop' into feature/WebApi"
    This reverts commit 2f75e6324a28c0940353dba91f3071dfd4f2f3e1.

--- Triangle Streaming Server/Program.cs
+++ Triangle Streaming Server/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WebSocketSharp.Server;
+using Microsoft.Owin.Hosting;
+using Triangle_Streaming_Server.WebApi;
 
 namespace Triangle_Streaming_Server
 {
