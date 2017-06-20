c5a9a47c65b7b1e40ab7b455c3355bbb65b37238
Hugo Zink <hugo.zink@hotmail.com>
2017-06-20 14:01:06 +0200

    Server can now receive streamer name from Android app.

--- Triangle Streaming Server/Models/VideoStream.cs
+++ Triangle Streaming Server/Models/VideoStream.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TriangleStreamingServer.Extensions;
 using TriangleStreamingServer.WebSockets;
+using Newtonsoft.Json;
 
 namespace TriangleStreamingServer.Models
 {
@@ -80,6 +81,22 @@ namespace TriangleStreamingServer.Models
 
 							StreamManager.Streams[socketId].LatestSignature = decodedSignature;
 							return;
+						}
+						else if(data.StartsWith("{") && data.EndsWith("}"))
+						{
+							//The definition for the expected JSON object.
+							var streamInfoDefinition = new { StreamerName = "" };
+
+							try
+							{
+								//Extract stream title and streamer name from JSON, and put it into the streamer's Stream object.
+								var streamInfo = JsonConvert.DeserializeAnonymousType(data, streamInfoDefinition);
+								StreamManager.Streams[socketId].StreamerName = streamInfo.StreamerName;
+							}
+							catch(JsonReaderException e)
+							{
+								Console.WriteLine("Received invalid JSON: {0}", data);
+							}
 						}
 
 						break;
