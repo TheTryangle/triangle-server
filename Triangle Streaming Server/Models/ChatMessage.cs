6078d9774a7c61d235e3bde25da9d01587eebae9
Marco <marcohavermans+github@hotmail.nl>
2017-06-20 15:56:38 +0200

    Changed setup of the chatStream and chatmesssage model.

---
+++ Triangle Streaming Server/Models/ChatMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TriangleStreamingServer.Models
+{
+	public class ChatMessage
+	{
+		public string StreamId { get; set; }
+		public DateTime Timestamp { get; set; }
+		public string Message { get; set; }
+	}
+}
--- Triangle Streaming Server/Models/ChatStream.cs
+++ Triangle Streaming Server/Models/ChatStream.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,74 +13,59 @@ using TriangleStreamingServer.WebSockets;
 
 namespace TriangleStreamingServer.Models
 {
-    public class ChatStream : WebSockets.WebSocketHandler
-    {
-        private static string _name;
+	public class ChatStream : WebSockets.WebSocketHandler
+	{
+		private static string _name;
 
-        
+		public ChatStream(WebSocketConnectionManager webSocketConnectionManager, StreamQueueManager streamManager) : base(webSocketConnectionManager, streamManager)
+		{
+			Clients = new ConcurrentDictionary<string, List<string>>();
+		}
 
-        public ChatStream(WebSocketConnectionManager webSocketConnectionManager, StreamQueueManager streamManager) : base(webSocketConnectionManager, streamManager)
-        {
-            Clients = new Dictionary<string, string>();
-        }
+		/// <summary>
+		/// Own ID, List of streamerIds
+		/// </summary>
+		public ConcurrentDictionary<string, List<string>> Clients { get; private set; }
 
-        public static Dictionary<string, string> Clients { get; private set; }
+		public override async Task OnConnected(WebSocket socket)
+		{
+			await base.OnConnected(socket);
+			Console.WriteLine("Someone Connected");
+		}
 
-        public override async Task OnConnected(WebSocket socket)
-        {
-            await base.OnConnected(socket);
+		public override Task OnDisconnected(WebSocket socket)
+		{
+			string id = WebSocketConnectionManager.GetId(socket);
 
-            var socketId = WebSocketConnectionManager.GetId(socket);
-            Console.WriteLine("Someone Connected");
-        }
+			Clients.TryRemove(id, out List<string> removedValue);
+			return base.OnDisconnected(socket);
 
-        public override Task OnDisconnected(WebSocket socket)
-        {
-            string id = WebSocketConnectionManager.GetId(socket);
+		}
 
-            Clients.Remove(id);
-            return base.OnDisconnected(socket);
-            Console.WriteLine("Someone disconnected");
-            //Sessions.Broadcast(String.Format("{0} logged off...", _name));
+		public override async Task OnMessage(WebSocket socket, WebSocketReceiveResult result, WebSocketMessageType type, byte[] buffer)
+		{
+			if (type != WebSocketMessageType.Text)
+			{
+				throw new NotImplementedException();
+			}
+			string data = Encoding.UTF8.GetString(buffer).Trim('\0');
+			string id = WebSocketConnectionManager.GetId(socket);
+			Console.WriteLine(data);
 
-        }
+			ChatMessage sendMessage = JsonConvert.DeserializeObject<ChatMessage>(data);
+			if (!Clients.ContainsKey(id))
+			{
+				// Not chatting
+				Clients.TryAdd(id, new List<string> { sendMessage.StreamId });
+			}
+			else
+			{
+				Clients[id].Add(sendMessage.StreamId);
+			}
 
-        public override async Task OnMessage(WebSocket socket, WebSocketReceiveResult result, WebSocketMessageType type, byte[] buffer)
-        {
-            if (type != WebSocketMessageType.Text)
-            {
-                throw new NotImplementedException();
-            }
-            string data = Encoding.UTF8.GetString(buffer).Trim('\0');
-            string id = WebSocketConnectionManager.GetId(socket);
-            Console.WriteLine(data);
 
-            if (data.StartsWith("JOIN"))
-            {
-                //Get data from json
-                var json = data.Replace("JOIN ", "");
-                var jsonData = (JObject)JsonConvert.DeserializeObject(json);
-                string streamid = jsonData["StreamID"].ToString();
-
-                if (Clients.ContainsKey(id))
-                {
-                    Clients.Remove(id);
-                }
-
-                Clients.Add(id, streamid);
-
-                var _list = Clients.Where(p => p.Value == streamid).Select(p => p.Key).ToArray();
-                //await SendToAll(json, WebSocketConnectionManager.GetAll().Keys.ToArray());
-                await SendToAll(json, _list);
-            }
-            else if (data.StartsWith("NAME"))
-            {
-                var json = data.Replace("NAME ", "");
-                _name = json;
-                await SendToAll(String.Format("{0} logged in...", _name, WebSocketConnectionManager.GetAll().Keys.ToArray()));
-
-            }
-            
-        }
-    }
+			var _list = Clients.Where(p => p.Value.Contains(sendMessage.StreamId) && p.Key != id).Select(p => p.Key).ToArray();
+			await SendToAll(data, _list);
+		}
+	}
 }
